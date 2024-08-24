194e21c0929e861b1da2660ba4fc7e70d328b9d2
Israel Silva Fonseca <132410269+IsraPrime16@users.noreply.github.com>
2024-08-24 16:46:06 -0300

    Add files via upload

---
+++ Soma.js
@@ -0,0 +1,12 @@
+// Função para somar dois números
+function somarNumeros(a, b) {
+    return a + b;
+}
+
+// Chamada da função
+let numero1 = 5;
+let numero2 = 10;
+let resultado = somarNumeros(numero1, numero2);
+
+// Exibe o resultado no console
+console.log("O resultado da soma é: " + resultado);
